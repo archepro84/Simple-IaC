d8560b38604f74c8ee832b29d6ec7ad07fe92982
archepro84 <archepro84@gmail.com>
2022-04-24 00:57:26 +0900

    feat: Add ubuntu 18.04 t2.nano ec2 instance

---
+++ terraform/vpc/vpc-create/ec2-public.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "vpc-create-public-ec2" {
+  ami           = "ami-0ed11f3863410c386" # ubuntu 18.04 amd64
+  instance_type = "t2.nano"
+  key_name = aws_key_pair.vpc-create-generated-key.key_name
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.vpc-create-public-ec2-ni.id
+  }
+}
+
+resource "aws_eip" "vpc-create-public-eip" {
+  instance = aws_instance.vpc-create-public-ec2.id
+  vpc      = true
+}
