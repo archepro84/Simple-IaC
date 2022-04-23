6836160491d8c8c620623d461d46b3ef3ef95518
archepro84 <archepro84@gmail.com>
2022-04-23 20:03:31 +0900

    feat: Add vpc-create terraform init

---
+++ terraform/vpc/vpc-create/vpc.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "vpc-create-main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "${local.SERVICE_NAME}-create"
+  }
+}
+
+resource "aws_internet_gateway" "vpc-create-ig" {
+  vpc_id = aws_vpc.vpc-create-main.id
+
+  tags = {
+    Name = "${local.SERVICE_NAME}-create-ig"
+  }
+}
