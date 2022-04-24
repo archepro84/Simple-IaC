10f6d427f503ed6e50d6b5773d234fedae9cf51f
archepro84 <archepro84@gmail.com>
2022-04-24 16:44:48 +0900

    feat: Add route settings from subnet

--- terraform/vpc/vpc-create/subnet-private.tf
+++ terraform/vpc/vpc-create/subnet-private.tf
@@ -6,9 +6,15 @@ resource "aws_route_table" "vpc-create-private-rtb" {
   }
 }
 
+resource "aws_route_table_association" "vpc-create-private-route-table-association"{
+  subnet_id = aws_subnet.vpc-create-private-subnet.id
+  route_table_id = aws_route_table.vpc-create-private-rtb.id
+}
+
 resource "aws_subnet" "vpc-create-private-subnet" {
   vpc_id     = aws_vpc.vpc-create-main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "${local.REGION}c"
 
   tags = {
     Name = "${local.SERVICE_NAME}-create-private-subnet"
--- terraform/vpc/vpc-create/subnet-public.tf
+++ terraform/vpc/vpc-create/subnet-public.tf
@@ -1,3 +1,7 @@
+resource "aws_default_route_table" "cpv-create-default-rt"{
+  default_route_table_id = aws_vpc.vpc-create-main.default_route_table_id
+}
+
 resource "aws_route_table" "vpc-create-public-rtb" {
   vpc_id = aws_vpc.vpc-create-main.id
   route {
@@ -10,9 +14,21 @@ resource "aws_route_table" "vpc-create-public-rtb" {
   }
 }
 
+resource "aws_route" "vpc-create-route" {
+  route_table_id = aws_vpc.vpc-create-main.default_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.vpc-create-ig.id
+}
+
+resource "aws_route_table_association" "vpc-create-public-route-table-association"{
+  subnet_id = aws_subnet.vpc-create-public-subnet.id
+  route_table_id = aws_route_table.vpc-create-public-rtb.id
+}
+
 resource "aws_subnet" "vpc-create-public-subnet" {
   vpc_id     = aws_vpc.vpc-create-main.id
   cidr_block = "10.0.0.0/24"
+  availability_zone = "${local.REGION}a"
 
   tags = {
     Name = "${local.SERVICE_NAME}-create-public-subnet"
