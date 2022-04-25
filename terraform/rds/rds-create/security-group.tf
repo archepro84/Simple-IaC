401d49c86b1e9d6dc34c853a5ba7c0b369277a16
archepro84 <archepro84@gmail.com>
2022-04-26 00:22:42 +0900

    feat: Add network interface from rds-create

---
+++ terraform/rds/rds-create/security-group.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "rds-create-security-group" {
+  name        = "rds-create-security-group"
+  description = "rds-create-security-group"
+  vpc_id      = aws_vpc.rds-create-main.id
+}
+
+resource "aws_security_group_rule" "rds-create-security-group-rule-mysql-aurora" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.rds-create-security-group.id
+}
+
+resource "aws_security_group_rule" "rds-create-security-group-rule-self" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = [aws_security_group.rds-create-security-group.id]
+  security_group_id = aws_security_group.rds-create-security-group.id
+}
+
+resource "aws_security_group_rule" "rds-create-security-group-rule-egress-all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.rds-create-security-group.id
+}
+
+resource "aws_network_interface" "rds-create-rds-ni" {
+  subnet_id       = aws_subnet.rds-create-subnet.id
+  security_groups = [aws_security_group.rds-create-security-group.id]
+
+  tags = {
+    Name = "${local.SERVICE_NAME}-create-ni"
+  }
+}
