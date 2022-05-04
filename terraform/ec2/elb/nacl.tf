6fd43903549b731950223c19f21ad8b741c69c24
ArchaeG <archepro84@gmail.com>
2022-05-04 21:31:56 +0900

    feat: Add network interface

---
+++ terraform/ec2/elb/nacl.tf
@@ -0,0 +1,26 @@
+resource "aws_network_acl_rule" "elb-nacl-ingress-all" {
+  network_acl_id = aws_vpc.elb-main.default_network_acl_id
+  rule_number    = 100
+  rule_action    = "allow"
+  egress         = false
+  from_port      = 0
+  to_port        = 0
+  protocol       = "-1"
+  cidr_block     = "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "elb-nacl-egress-all" {
+  network_acl_id = aws_vpc.elb-main.default_network_acl_id
+  rule_number    = 100
+  rule_action    = "allow"
+  egress         = true
+  from_port      = 0
+  to_port        = 0
+  protocol       = "-1"
+  cidr_block     = "0.0.0.0/0"
+}
+
+resource "aws_network_acl_association" "elb-nacl-association" {
+  network_acl_id = aws_vpc.elb-main.default_network_acl_id
+  subnet_id = aws_subnet.elb-subnet.id
+}
---
+++ terraform/ec2/elb/security-group.tf
@@ -0,0 +1,50 @@
+resource "aws_security_group" "elb-security-group" {
+  name        = "elb-security-group"
+  description = "elb-security-group"
+  vpc_id      = aws_vpc.elb-main.id
+}
+
+resource "aws_security_group_rule" "elb-security-group-rule-ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.elb-security-group.id
+}
+
+resource "aws_security_group_rule" "elb-security-group-rule-http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.elb-security-group.id
+}
+
+resource "aws_security_group_rule" "elb-security-group-rule-https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.elb-security-group.id
+}
+
+resource "aws_security_group_rule" "elb-security-group-rule-egress-all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.elb-security-group.id
+}
+
+resource "aws_network_interface" "elb-ec2-ni" {
+  subnet_id       = aws_subnet.elb-subnet.id
+  security_groups = [aws_security_group.elb-security-group.id]
+
+  tags = {
+    Name = "${local.SERVICE_NAME}-ec2-ni"
+  }
+}
