59997cb6e9e71d4810da872138f3387e8f0d1694
ArchaeG <archepro84@gmail.com>
2022-05-04 21:32:13 +0900

    feat: Add ev2 instance

---
+++ terraform/ec2/elb/ec2-one.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "elb-ec2-one" {
+  ami           = "ami-0ed11f3863410c386" # ubuntu 18.04 amd64
+  instance_type = "t2.nano"
+  key_name = aws_key_pair.elb-generated-key.key_name
+  availability_zone = aws_subnet.elb-subnet.availability_zone
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.elb-ec2-ni.id
+  }
+}
