37f5a1367ad4f7eeb8ed5cf84ff49e8b8df8bab0
archepro84 <archepro84@gmail.com>
2022-05-05 23:07:05 +0900

    chore: Add config variables

--- terraform/ec2/elb/variables.tf
+++ terraform/ec2/elb/variables.tf
@@ -3,8 +3,10 @@ locals {
   REGION       = local.config.REGION
   SERVICE_NAME = local.config.SERVICE_NAME
   KEY_NAME     = local.config.KEY_NAME
+  SERVER_PORT  = local.config.SERVER_PORT
+  ROUTE53_URI  = local.config.ROUTE53_URI
 
-  ACCOUNT_ID   = data.aws_caller_identity.current.account_id
+  ACCOUNT_ID = data.aws_caller_identity.current.account_id
 }
 
 # User 정보 호출
