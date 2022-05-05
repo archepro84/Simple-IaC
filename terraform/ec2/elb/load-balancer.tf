2893152047777e955076353f7a38e7ade88ee6d7
archepro84 <archepro84@gmail.com>
2022-05-05 23:08:07 +0900

    feat: Add Load Balancer

---
+++ terraform/ec2/elb/load-balancer.tf
@@ -0,0 +1,15 @@
+resource "aws_lb" "elb-lb" {
+  name               = "${local.SERVICE_NAME}-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.elb-security-group-one.id, aws_security_group.elb-security-group-two.id]
+  subnets            = [aws_subnet.elb-subnet-one.id, aws_subnet.elb-subnet-two.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Name = "${local.SERVICE_NAME}-lb"
+  }
+
+}
+
