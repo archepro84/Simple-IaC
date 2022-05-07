68a8fbcfb61ed0605d20bba180f0b773a827fc21
archepro84 <archepro84@gmail.com>
2022-05-07 20:28:34 +0900

    feat: Add user_data and monitoring config from ec2 instance

--- terraform/ec2/elb/ec2-two.tf
+++ terraform/ec2/elb/ec2-two.tf
@@ -1,11 +1,18 @@
 resource "aws_instance" "elb-ec2-two" {
-  ami           = "ami-0ed11f3863410c386" # ubuntu 18.04 amd64
-  instance_type = "t2.nano"
-  key_name = aws_key_pair.elb-generated-key.key_name
+  ami               = "ami-0ed11f3863410c386" # ubuntu 18.04 amd64
+  instance_type     = "t2.nano"
+  key_name          = aws_key_pair.elb-generated-key.key_name
   availability_zone = aws_subnet.elb-subnet-two.availability_zone
+  monitoring        = true
+
+  user_data = file("./git-pull-project.sh")
 
   network_interface {
     device_index         = 0
     network_interface_id = aws_network_interface.elb-ec2-two-ni.id
+  }
+
+  tags = {
+    Name = "${local.SERVICE_NAME}-ec2-two"
   }
 }
