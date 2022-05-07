52ce4ae2d6d7743a8ee888db7a46339d89fa26cb
archepro84 <archepro84@gmail.com>
2022-05-07 14:12:31 +0900

    fix: Fix internet gateway destroy error

--- terraform/ec2/elb/load-balancer.tf
+++ terraform/ec2/elb/load-balancer.tf
@@ -5,7 +5,7 @@ resource "aws_lb" "elb-lb" {
   security_groups    = [aws_security_group.elb-security-group-elb.id]
   subnets            = [aws_subnet.elb-subnet-one.id, aws_subnet.elb-subnet-two.id]
 
-  enable_deletion_protection = true
+  #  enable_deletion_protection = true
 
   tags = {
     Name = "${local.SERVICE_NAME}-lb"
