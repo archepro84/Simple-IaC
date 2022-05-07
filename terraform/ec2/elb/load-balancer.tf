69653aebf9647f355f2abce9e3145a6e30435eff
archepro84 <archepro84@gmail.com>
2022-05-07 12:40:38 +0900

    feat: Change loadbalancer secutiry groups

--- terraform/ec2/elb/load-balancer.tf
+++ terraform/ec2/elb/load-balancer.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "elb-lb" {
   name               = "${local.SERVICE_NAME}-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.elb-security-group-one.id, aws_security_group.elb-security-group-two.id]
+  security_groups    = [aws_security_group.elb-security-group-elb.id]
   subnets            = [aws_subnet.elb-subnet-one.id, aws_subnet.elb-subnet-two.id]
 
   enable_deletion_protection = true
