28b6a9f690b8f65bb6abf1e7eb2bd149e9d7d841
archepro84 <archepro84@gmail.com>
2022-05-09 23:32:02 +0900

    feat: Add route53

--- terraform/route53/route53-eip/route53.tf
+++ terraform/route53/route53-eip/route53.tf
@@ -1,1 +1,22 @@
-  type ="CNAME"
+resource "aws_route53_zone" "route53-eip-route53-zone" {
+  name = local.ROUTE53_URI
+}
+
+
+resource "aws_route53_record" "route53-eip-route53-record-http" {
+  zone_id = aws_route53_zone.route53-eip-route53-zone.id
+
+  name    = "${local.ROUTE53_URI}"
+  type    = "A"
+  ttl     = 20
+  records = [aws_eip.route53-eip-eip.public_ip]
+}
+
+resource "aws_route53_record" "route53-eip-route53-record-www" {
+  zone_id = aws_route53_zone.route53-eip-route53-zone.id
+
+  name    = "www.${local.ROUTE53_URI}"
+  type    = "CNAME"
+  ttl     = 20
+  records = [aws_route53_record.route53-eip-route53-record-http.name]
+}
