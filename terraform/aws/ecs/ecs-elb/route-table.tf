158aea560326fba0e83789a205a42eb7f6312cf5
archepro84 <archepro84@gmail.com>
2022-05-23 23:45:33 +0900

    feat: Add route table asssociation for subnets

--- terraform/aws/ecs/ecs-elb/route-table.tf
+++ terraform/aws/ecs/ecs-elb/route-table.tf
@@ -11,3 +11,13 @@ resource "aws_route" "ecs-elb-rtb-route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.ecs-elb-igw.id
 }
+
+resource "aws_route_table_association" "ecs-elb-route-table-association-1" {
+  subnet_id      = aws_subnet.ecs-elb-subnet-1.id
+  route_table_id = aws_route_table.ecs-elb-rtb.id
+}
+
+resource "aws_route_table_association" "ecs-elb-route-table-association-2" {
+  subnet_id      = aws_subnet.ecs-elb-subnet-2.id
+  route_table_id = aws_route_table.ecs-elb-rtb.id
+}
