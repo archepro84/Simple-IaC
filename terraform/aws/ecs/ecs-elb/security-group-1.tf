b9cd6e13fe5a950ec94e932addb9e0aa2c4b5ba6
archepro84 <archepro84@gmail.com>
2022-05-24 00:35:01 +0900

    feat: Add Security-group

---
+++ terraform/aws/ecs/ecs-elb/security-group-1.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "ecs-elb-security-group-1" {
+  name        = "${local.SERVICE_NAME}-security-group-1"
+  description = "${local.SERVICE_NAME}-security-group-1 Description"
+  vpc_id      = aws_vpc.ecs-elb-main.id
+
+  tags = {
+    Name = "${local.SERVICE_NAME}-security-group-1"
+  }
+}
+
+resource "aws_security_group_rule" "ecs-elb-security-group-rule-server-1" {
+  type              = "ingress"
+  from_port         = local.SERVER_PORT
+  to_port           = local.SERVER_PORT
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ecs-elb-security-group-1.id
+}
+
+resource "aws_security_group_rule" "ecs-elb-security-group-rule-egress-all-1" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ecs-elb-security-group-1.id
+}
+
+resource "aws_network_interface" "ecs-elb-ni-1" {
+  subnet_id       = aws_subnet.ecs-elb-subnet-1.id
+  security_groups = [aws_security_group.ecs-elb-security-group-1.id]
+
+  tags = {
+    Name = "${local.SERVICE_NAME}-ni-1"
+  }
+}
