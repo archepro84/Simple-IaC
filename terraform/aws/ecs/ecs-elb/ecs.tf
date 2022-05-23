e7bc14c849e03659c179a34339558ce7591a4824
archepro84 <archepro84@gmail.com>
2022-05-23 23:20:38 +0900

    chore: Rename Service name

--- terraform/aws/ecs/ecs-elb/ecs.tf
+++ terraform/aws/ecs/ecs-elb/ecs.tf
@@ -1,10 +1,10 @@
-resource "aws_ecs_task_definition" "ecs-ecs-task-definition" {
+resource "aws_ecs_task_definition" "ecs-elb-ecs-task-definition" {
   family                = "service"
   container_definitions = jsonencode([
     {
       name         = "express-one"
       image        = "service-one"
-      cpu          = 2
+      cpu          = 256
       memory       = 256
       essential    = true
       portMappings = [
@@ -27,11 +27,11 @@ resource "aws_ecs_task_definition" "ecs-ecs-task-definition" {
   }
 }
 
-resource "aws_ecs_cluster" "ecs-ecs-cluster" {
+resource "aws_ecs_cluster" "ecs-elb-ecs-cluster" {
   name = "${local.SERVICE_NAME}-ecs-cluster"
 }
 
-resource "aws_ecs_service" "ecs-ecs-service" {
+resource "aws_ecs_service" "ecs-elb-ecs-service" {
   name    = "${local.SERVICE_NAME}-esc-service"
-  cluster = aws_ecs_cluster.ecs-ecs-cluster.id
+  cluster = aws_ecs_cluster.ecs-elb-ecs-cluster.id
 }
