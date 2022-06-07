dd41fcb657e0eecb6ebadb1d47214d243d50e7e9
archepro84 <archepro84@gmail.com>
2022-06-07 09:07:21 +0900

    feat: Add codebuild source and credential

--- terraform/aws/code-pipeline/code-build/code-build.tf
+++ terraform/aws/code-pipeline/code-build/code-build.tf
@@ -1,11 +1,33 @@
-#GITHUB_ACCESS_TOKEN
-
-resource "aws_codebuild_project" "code-build-codebuild-project"{
-  name = "${local.SERVICE_NAME}-codebuild-project"
-  description = "${local.SERVICE_NAME}-codebuild-project-description"
+resource "aws_codebuild_project" "code-build-codebuild-project" {
+  name         = "${local.SERVICE_NAME}-codebuild-project"
+  description  = "${local.SERVICE_NAME}-codebuild-project-description"
   service_role = aws_iam_role.code-build-iam-role.arn
 
   artifacts {
     type = "NO_ARTIFACTS"
   }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:1.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/archepro84/prac-docker.git"
+    git_clone_depth = 1
+  }
+  source_version = "master"
+
+  tags = {
+    Name = "${local.SERVICE_NAME}-codebuild-project"
+  }
+}
+
+resource "aws_codebuild_source_credential" "code-build-codebuild-source-credential" {
+  auth_type   = "PERSONAL_ACCESS_TOKEN"
+  server_type = "GITHUB"
+  token       = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["GITHUB_ACCESS_TOKEN"]
 }
