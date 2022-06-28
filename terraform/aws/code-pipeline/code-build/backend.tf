df8bbfab4fdf41925b423f4026a16a6996a6c852
ArchaeG <archepro84@gmail.com>
2022-06-29 00:56:14 +0900

    feat: Remove backend.tf with ignore project file

--- terraform/aws/code-pipeline/code-build/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "simple-iac-tfstate"
-    key            = "Simple-IaC/code-pipeline/code-build/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "simple-iac-tfstate-lock"
-  }
-}
--- terraform/aws/ec2/elb/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "simple-iac-tfstate"
-    key            = "Simple-IaC/ec2/elb/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "simple-iac-tfstate-lock"
-  }
-}
