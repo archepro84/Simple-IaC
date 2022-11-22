a456df5bc74e31f311888499ddf1f14188bf2c05
ArchaeG <archepro84@gmail.com>
2022-11-22 23:26:27 +0900

    feat: Add terraform variables by get-secret project

---
+++ terraform/aws/secrets-manager/get-secret/Simple-IaC.get-secret.auto.tfvars
@@ -0,0 +1,3 @@
+service_name    = "get-secret"
+region          = "ap-northeast-2"
+aws_secret_name = "Simple-IaC-Secrets"
---
+++ terraform/aws/secrets-manager/get-secret/variables.tf
@@ -0,0 +1,11 @@
+variable "service_name" {
+  description = "Terraform Service Name"
+}
+
+variable "region" {
+  description = "AWS Region"
+}
+
+variable "aws_secret_name" {
+  description = "AWS Project Secret Key Name"
+}
