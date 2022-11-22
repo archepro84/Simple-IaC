c095aafdfddd96bbe70929939ab3c690af3ef5e1
ArchaeG <archepro84@gmail.com>
2022-11-22 23:30:48 +0900

    fix: Fix deprecated local_file to local_sensitive_file

--- terraform/aws/secrets-manager/get-secret/secrets-manager.tf
+++ terraform/aws/secrets-manager/get-secret/secrets-manager.tf
@@ -1,7 +1,7 @@
 # name과 일치하는 AWS Secrets Manager의 Secret을 가져옵니다.
 # docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/secretsmanager_secret
 data "aws_secretsmanager_secret" "secrets" {
-  name = var.aws_secrets_name
+  name = var.aws_secret_name
 }
 
 
@@ -9,22 +9,10 @@ data "aws_secretsmanager_secret_version" "current" {
   secret_id = data.aws_secretsmanager_secret.secrets.id
 }
 
-#output "aws_secretsmanager_secret" {
-#  value = data.aws_secretsmanager_secret.secrets
-#
-#  sensitive = true # Sensitive한 값이더라도 출력합니다.
-#}
-#
-#output "aws_secretsmanager_secret_version" {
-#  value = data.aws_secretsmanager_secret_version.current
-#
-#  sensitive = true # Sensitive한 값이더라도 출력합니다.
-#}
 
-
-# Secrets Manager의 Key를 Json 파일로 출력합니다.
-resource "local_file" "secrets" {
-  filename          = "./secrets.json"
-  file_permission   = "400"
-  sensitive_content = data.aws_secretsmanager_secret_version.current.secret_string
-}
+# # Secrets Manager의 Key를 Json 파일로 출력합니다.
+#resource "local_sensitive_file" "secrets" {
+#  filename        = "./secrets.json"
+#  content         = data.aws_secretsmanager_secret_version.current.secret_string
+#  file_permission = "0400" # Linux mode
+#}
