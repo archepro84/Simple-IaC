a198f2b5f4200567bb7d5e26edf849a1e0fa0588
ArchaeG <archepro84@gmail.com>
2022-11-24 01:34:01 +0900

    feat: Add terraform cloud backend by local execution mode

--- terraform/aws/code-pipeline/code-commit/main.tf
+++ terraform/aws/code-pipeline/code-commit/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "simple-iac"
+
+    workspaces {
+      name = "code-commit"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
